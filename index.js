a923013924825ed4c051f407d293eb86f5d6e7b7
John Elison Duero <johnelison_duero@yahoo.com>
2023-05-10 11:23:12 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+tailwind.config = {
+    darkMode: 'class',
+    theme: {
+        extend: {
+            colors: {
+                "logo": {
+                    100: '#1fb6ff',
+                    200: '#7e5bef',
+                    300: '#ff49db',
+                    400: '#ff7849',
+                    500: '#13ce66',
+                    600: '#ffc82c',
+                    700: '#273444',
+                    800: '#8492a6',
+                    900: '#d3dce6',
+                }
+            }
+        }
+    }
+}
